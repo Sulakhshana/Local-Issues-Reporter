ac80ff857b00a716e9346608cff1467e735e98b4
Sulakhshana <124502474+Sulakhshana@users.noreply.github.com>
2025-04-05 11:49:25 +0530

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -3,13 +3,13 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0...", 
-  authDomain: "civic-issue-reporter.firebaseapp.com", 
-  projectId: "civic-issue-reporter", 
-  storageBucket: "civic-issue-reporter.appspot.com", 
-  messagingSenderId: "415273797037", 
-  appId: "1:415273797037:web:304c4d1625c32d3282e6fd", 
-  measurementId: "G-W1TXWGVB3C" 
+  apiKey: "AIzaSyD0...",
+  authDomain: "civic-issue-reporter.firebaseapp.com",
+  projectId: "civic-issue-reporter",
+  storageBucket: "civic-issue-reporter.appspot.com",
+  messagingSenderId: "415273797037",
+  appId: "1:415273797037:web:304c4d1625c32d3282e6fd",
+  measurementId: "G-W1TXWGVB3C"
 };
 
 const app = initializeApp(firebaseConfig);
